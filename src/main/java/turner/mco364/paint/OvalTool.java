6a15b4229e366c4279e38c3cef8b9bf919a4849c
chayaturner <dietaholic@gmail.com>
2016-02-24 18:16:30 -0500

    added oval tool

--- src/main/java/turner/mco364/paint/OvalTool.java
+++ src/main/java/turner/mco364/paint/OvalTool.java
@@ -1,27 +1,46 @@
 package turner.mco364.paint;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
-public class OvalTool implements Tool{
+public class OvalTool implements Tool {
 
+	private int x1, y1;
+	private int x2, y2;
+	private int width, height;
+
+	@Override
 	public void mousePressed(Graphics g, int x, int y) {
-		// TODO Auto-generated method stub
-		
+		x1 = x;
+		y1 = y;
+		x2 = x;
+		y2 = y;
+		width = 0;
+		height = 0;
 	}
 
+	@Override
 	public void mouseReleased(Graphics g, int x, int y) {
-		// TODO Auto-generated method stub
-		
+		g.setColor(Color.RED);
+		x2 = x;
+		y2 = y;
+		width = Math.abs(x2 - x1);
+		height = Math.abs(y2 - y1);
+		g.drawOval(Math.min(x1, x2), Math.min(y1, y2), width, height);
 	}
 
+	@Override
 	public void mouseDragged(Graphics g, int x, int y) {
-		// TODO Auto-generated method stub
-		
+		x2 = x;
+		y2 = y;
+		width = Math.abs(x2 - x1);
+		height = Math.abs(y2 - y1);
 	}
 
+	@Override
 	public void drawPreview(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		g.setColor(Color.RED);
+		g.drawOval(Math.min(x1, x2), Math.min(y1, y2), width, height);
 	}
 
 }
